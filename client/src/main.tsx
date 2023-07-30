ec110ad404a2260b8fbf15f9d4d78cb8513ec964
Asus Laptop <minhnle311@gmail.com>
2023-07-30 16:18:08 -0700

    Add date fns

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,10 +5,15 @@ import { Provider } from 'react-redux'
 import store from './app/store'
 import '../src/styles/index.css'
 
+import { LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
       <RouteSwitch />
+      </LocalizationProvider>
     </Provider>
   </React.StrictMode>,
 )
