4ffe55fba4f0116a5e7415e8ef4700d67871d21d
Asus Laptop <minhnle311@gmail.com>
2023-08-07 14:39:13 -0700

    Implement service worker

---
+++ client/src/serviceWorker.ts
@@ -0,0 +1,38 @@
+self.addEventListener('message', async (e) => {
+    const {data, id} = e.data
+
+    const chunks = [].concat(...data).map(url => dataUrltoBlob(url))
+
+    const formData = new FormData()
+
+    chunks.forEach((chunk, index) => {
+        console.log(index)
+        formData.append(`sprays[]`, chunk, index.toString())
+    })
+
+    await fetch('http://127.0.0.1:8001/upload', {
+            credentials: 'include',
+            method: 'POST',
+            headers: {
+                'X-Upload-Id': id,
+            },
+            body: formData
+        })
+
+    self.postMessage('Done')
+})
+
+const dataUrltoBlob = (url: string) => {
+    const parts = url.split(';base64,')
+
+    const mimeType = parts[0].split(':')[1]
+    const data = atob(parts[1])
+
+    const uint8Array = new Uint8Array(data.length)
+
+    for (let i = 0; i < data.length; i++) {
+        uint8Array[i] = data.charCodeAt(i)
+    }
+
+    return new Blob([uint8Array], {type: mimeType})
+}
