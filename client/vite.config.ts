06cdbe879da8a68d3cbaddf0e9c8539e257c41c7
Asus Laptop <minhnle311@gmail.com>
2023-08-07 14:38:28 -0700

    Change favicon and tittle

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name]-[hash].js',
+        chunkFileNames: '[name]-[hash].js',
+        format: 'iife',
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['../../serviceWorker'], // Include the Web Worker for optimization
+  },
 })
